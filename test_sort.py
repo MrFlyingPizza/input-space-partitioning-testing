de450153646556f7de04efae5a65814c27a779c3
Richard Gao <hga38@sfu.ca>
2026-02-20 22:17:02 -0800

    Add pytest tests

---
+++ test_sort.py
@@ -0,0 +1,19 @@
+import csv
+from collections import namedtuple
+
+import pytest
+
+
+def get_test_case(filename):
+    with open(filename) as tests_file:
+        csv_reader = csv.reader(filter(lambda row: row[0] != "#", tests_file))
+        headers = next(csv_reader)
+        TestCase = namedtuple("TestCase", headers)
+        for row in csv_reader:
+            yield TestCase(*row)
+
+
+@pytest.mark.parametrize("test_case", list(get_test_case("params-output.csv")))
+def test_eval(test_case):
+    print(test_case)
+    assert True

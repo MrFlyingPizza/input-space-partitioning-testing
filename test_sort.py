1237aae9e011bf0a0a1cc4f62b29b0408b8e87c0
Richard Gao <hga38@sfu.ca>
2026-02-20 22:22:57 -0800

    Remove list

--- test_sort.py
+++ test_sort.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 import pytest
 
 
-def get_test_case(filename):
+def get_test_cases(filename):
     with open(filename) as tests_file:
         csv_reader = csv.reader(filter(lambda row: row[0] != "#", tests_file))
         headers = next(csv_reader)
@@ -13,7 +13,7 @@ def get_test_case(filename):
             yield TestCase(*row)
 
 
-@pytest.mark.parametrize("test_case", list(get_test_case("params-output.csv")))
+@pytest.mark.parametrize("test_case", get_test_cases("params-output.csv"))
 def test_eval(test_case):
     print(test_case)
     assert True

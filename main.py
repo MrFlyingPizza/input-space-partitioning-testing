91a4451f40de65c94ec54fd0ba9df80d6cddc0eb
Richard Gao <hga38@sfu.ca>
2026-02-20 21:53:01 -0800

    Add conversion

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def main():...
+import csv
+from collections import namedtuple
+from typing import Iterator
+
+def main():
+    with open("params-output.csv") as tests_file:
+        csv_reader = csv.reader(filter(lambda row: row[0] != "#", tests_file))
+        headers = next(csv_reader)
+        TestCase = namedtuple("TestCase", headers)
+        for row in csv_reader:
+            test_case = TestCase(*row)
+            print(test_case)
 
 if __name__ == "__main__":
     main()

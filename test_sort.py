d1244a4e64bbd8b0e6ca7edabc83b1dbda645947
Richard Gao <hga38@sfu.ca>
2026-02-21 09:39:24 -0800

    Do not use namedtuple

--- test_sort.py
+++ test_sort.py
@@ -1,18 +1,23 @@
 import csv
-from collections import namedtuple
+from dataclasses import dataclass
 
 import pytest
 
 
-def get_test_cases(filename):
+@dataclass
+class ActsTestCase:
+    names: list[str]
+    values: list[str]
+
+
+def get_acts_test_cases(filename):
     with open(filename) as tests_file:
         csv_reader = csv.reader(filter(lambda row: row[0] != "#", tests_file))
-        headers = next(csv_reader)
-        TestCase = namedtuple("TestCase", headers)
-        return list(TestCase(*row) for row in csv_reader)
+        names = next(csv_reader)
+        return list(ActsTestCase(names, values) for values in csv_reader)
 
 
-@pytest.mark.parametrize("test_case", get_test_cases("params-output.csv"))
+@pytest.mark.parametrize("test_case", get_acts_test_cases("params-output.csv"))
 def test_eval(test_case):
     print(test_case)
     assert True

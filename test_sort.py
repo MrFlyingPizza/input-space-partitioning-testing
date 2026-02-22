67a19888fd9d6d957e74695597104c67672ac532
Richard Gao <hga38@sfu.ca>
2026-02-22 00:24:22 -0800

    Supply stdin

--- test_sort.py
+++ test_sort.py
@@ -1,17 +1,20 @@
 import pytest
-import runner
 import subprocess
-import contextlib
+from contextlib import chdir, nullcontext
 from tempfile import TemporaryDirectory
 
+from runner import Context, read_test_cases
 
-@pytest.mark.parametrize("test_case", runner.read_test_cases("params-output.csv"))
+
+@pytest.mark.parametrize("test_case", read_test_cases("params-output.csv"))
 def test_eval(test_case):
-    with TemporaryDirectory() as temp_dir, contextlib.chdir(temp_dir), runner.Context(
+    with TemporaryDirectory() as temp_dir, chdir(temp_dir), Context(
         test_case
-    ) as context:
+    ) as context, (
+        open(context.stdin_file.name) if context.stdin_file else nullcontext()
+    ) as stdin_file:
         args = context.args + context.get_filenames()
-        result = subprocess.run(["sort", *args], capture_output=True)
-        print(result.stderr)
+        result = subprocess.run(["sort", *args], stdin=stdin_file, capture_output=True)
+        print(result.stderr, result.stdout)
 
     assert True

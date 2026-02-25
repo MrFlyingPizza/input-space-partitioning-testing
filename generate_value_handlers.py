6cca36d3b3ec76c26a0ef7e2b9802bb1c95b0230
Richard Gao <hga38@sfu.ca>
2026-02-24 19:43:28 -0800

    Fix file reference and imports

--- generate_value_handlers.py
+++ generate_value_handlers.py
@@ -1,12 +1,12 @@
 from test_sort import *
 from pprint import pprint
 
-from runner import read_test_cases
+from testcase import read_test_cases
 
 
 def main():
     value_handlers = dict()
-    for names, values in read_test_cases("params-output.csv"):
+    for names, values in read_test_cases("tests.csv"):
         for name, value in zip(names, values):
             values = value_handlers.setdefault(name, dict())
             values.setdefault(value, None)
